cd4b2469103239e25f35325da7b95134e0b3e5b4
Ankush3104 <akashpandey4104@gmail.com>
2025-08-10 11:28:48 +0530

    Rectangle

---
+++ C Language/Rectangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int l,b,r;
+
+    printf("Enter the Length\n");
+    scanf("%d",&l);
+
+    printf("Enter the Breadth\n");
+    scanf("%d",&b);
+
+    r = l*b;
+
+    printf("The result is %d",r);
+
+    return 0;
+}
