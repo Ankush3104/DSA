0be65a54fef15b5a137759576bbfa667349c44a9
Ankush3104 <akashpandey4104@gmail.com>
2025-08-16 09:46:09 +0530

    #locigs

---
+++ C Language/operator/logical.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void)
+{
+    int a = 0, b = 0;
+
+    printf("The value of a and b is %d\n ",a&&b);
+    printf("The value of a and b is %d\n ",a||b);
+    printf("The value of not (a) is %d ",!a);
+
+    getch();
+    return 0;
+}
