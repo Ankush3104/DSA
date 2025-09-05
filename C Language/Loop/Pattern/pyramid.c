86c979482d2609799b2edc7678191a873d9c4289
Ankush3104 <akashpandey4104@gmail.com>
2025-09-05 22:16:41 +0530

    Pyramid_pattern

--- C Language/Loop/Pattern/pyramid.c
+++ C Language/Loop/Pattern/pyramid.c
@@ -12,7 +12,7 @@ int main() {
             printf(" ");
         }
         // print stars
-        for (int k = 1; k <=i; k++) {
+        for (int k = 1; k <= (2 * i - 1); k++) {
             printf("*");
         }
         printf("\n");
